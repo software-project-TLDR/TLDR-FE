4f3e80140f554eef28dc06a02b402c83c19aac8d
t3qquq <t3qquq@gmail.com>
2023-05-03 14:06:23 +0900

    [feat] mainpage titlebox init

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 300vh;
+`;
+const Titlebox = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  margin-top: 15vh;
+  padding: 20px 0 20px 0;
+`;
+const TitleboxTitle = styled.div`
+  color: grey;
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+`;
+const TitleboxDesc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  margin-top: 10px;
+  /* margin-bottom: 20px; */
+`;
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <Container>
+        <Titlebox>
+          <TitleboxTitle>Tl;Dr</TitleboxTitle>
+          <TitleboxDesc>인공지능 강의요약 프로젝트</TitleboxDesc>
+        </Titlebox>
+      </Container>
+    </>
+  );
 };
 
 export default Main;
