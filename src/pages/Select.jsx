c45fca8e0cc681d5468a938b8210e93a60ed5a52
t3qquq <t3qquq@gmail.com>
2023-05-09 19:01:18 +0900

    [feat] content box init

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -18,12 +18,32 @@ const TitleBoxTitle = styled.div`
   text-align: center;
 `;
 
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* text-align: center; */
+  /* background-color: red; */
+`;
+const ContentBox = styled.div`
+  width: 70vh;
+  height: 70vh;
+  background-color: yellow;
+`;
+
 const Select = () => {
   return (
     <Container>
       <TitleBox>
         <TitleBoxTitle> 파일 업로드 방식을 선택해주세요. </TitleBoxTitle>
       </TitleBox>
+      <ContentContainer>
+        <ContentBox></ContentBox>
+        or
+        <ContentBox></ContentBox>
+      </ContentContainer>
     </Container>
   );
 };
