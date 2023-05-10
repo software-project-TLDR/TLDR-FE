865799cb6535bdb3c12e744a6dd652c34bfbae81
t3qquq <t3qquq@gmail.com>
2023-05-10 09:13:52 +0900

    [feat] record button form init

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { FaMicrophoneAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
@@ -19,6 +21,7 @@ const TitleBoxTitle = styled.div`
 `;
 
 const ContentContainer = styled.div`
+  font-size: 2rem;
   width: 100%;
   height: 70vh;
   display: flex;
@@ -28,9 +31,25 @@ const ContentContainer = styled.div`
   /* background-color: red; */
 `;
 const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 70vh;
   height: 70vh;
-  background-color: yellow;
+  /* background-color: yellow; */
+`;
+const RecordButton = styled.button`
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+  width: 20rem;
+  height: 20rem;
+  border-radius: 10rem;
+  background-color: transparent;
+  border: 0.51rem solid black;
+  opacity: 0.8;
 `;
 
 const Select = () => {
@@ -40,7 +59,12 @@ const Select = () => {
         <TitleBoxTitle> 파일 업로드 방식을 선택해주세요. </TitleBoxTitle>
       </TitleBox>
       <ContentContainer>
-        <ContentBox></ContentBox>
+        <ContentBox>
+          <RecordButton>
+            <FaMicrophoneAlt size={150} />
+          </RecordButton>
+          <h3>녹음하기</h3>
+        </ContentBox>
         or
         <ContentBox></ContentBox>
       </ContentContainer>
