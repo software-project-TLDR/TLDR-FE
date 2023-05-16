1e19fc42bc825b4d09208150c18716ef2fd21879
t3qquq <t3qquq@gmail.com>
2023-05-16 18:12:32 +0900

    [feat] uploadpage format

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const TitleBox = styled.div`
+  margin-top: 50px;
+  width: 60%;
+  border-bottom: 2px solid grey;
+  padding-bottom: 10px;
+`;
+const TitleBoxTitle = styled.div`
+  font-size: 24px;
+  text-align: center;
+`;
+
+const ContentContainer = styled.div`
+  font-size: 2rem;
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* text-align: center; */
+  /* background-color: red; */
+`;
 const Upload = () => {
-  return <div>Upload</div>;
+  return (
+    <Container>
+      <TitleBox>
+        <TitleBoxTitle> 강의파일 업로드 </TitleBoxTitle>
+      </TitleBox>
+      <ContentContainer></ContentContainer>
+    </Container>
+  );
 };
 
 export default Upload;
