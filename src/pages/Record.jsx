43b8a644bcbd4cc3d825a488c2355a99185e3c5f
t3qquq <t3qquq@gmail.com>
2023-05-23 13:31:02 +0900

    [feat] 다운로드, 녹음시각화

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,129 +1,57 @@
 import React, { useState, useCallback } from "react";
 import styled from "styled-components";
+import { ReactMic } from "react-mic";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+const Container = styled.div``;
+const TitleBox = styled.div``;
+const TitleBoxTitle = styled.div``;
+const ContentContainer = styled.div``;
 
-  width: 100%;
-  /* height: 100vh; */
-`;
-const TitleBox = styled.div`
-  margin-top: 50px;
-  width: 60%;
-  border-bottom: 2px solid grey;
-  padding-bottom: 10px;
-`;
-const TitleBoxTitle = styled.div`
-  font-size: 24px;
-  text-align: center;
-`;
-const ContentContainer = styled.div`
-  font-size: 2rem;
-  width: 100%;
-  height: 70vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+const RecordButton = styled.button``;
+const PlayButton = styled.button``;
+const DownloadButton = styled.button``;
+const UploadButton = styled.button``;
 
 function Record() {
-  const [stream, setStream] = useState();
-  const [media, setMedia] = useState();
-  const [onRec, setOnRec] = useState(true);
-  const [source, setSource] = useState();
-  const [analyser, setAnalyser] = useState();
-  const [audioUrl, setAudioUrl] = useState();
-  const [disabled, setDisabled] = useState(true);
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordText, setRecordText] = useState("녹음 시작하기");
+  const [recordedBlob, setRecordedBlob] = useState(null);
 
-  const onRecAudio = () => {
-    setDisabled(true);
-
-    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
-    const analyser = audioCtx.createScriptProcessor(0, 1, 1);
-    setAnalyser(analyser);
-
-    function makeSound(stream) {
-      // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
-      const source = audioCtx.createMediaStreamSource(stream);
-      setSource(source);
-      source.connect(analyser);
-      analyser.connect(audioCtx.destination);
+  const handleRecord = useCallback(() => {
+    if (!isRecording) {
+      setIsRecording(true);
+      setRecordText("녹음 중단하기");
+    } else {
+      setIsRecording(false);
+      setRecordText("녹음 시작하기");
+      setRecordedBlob(null); // 녹음 중단 시 recordedBlob 초기화
     }
-    // 마이크 사용 권한 획득
-    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-      setStream(stream);
-      setMedia(mediaRecorder);
-      makeSound(stream);
+  }, [isRecording]);
 
-      analyser.onaudioprocess = function (e) {
-        // 3시간(10800초) 지나면 자동으로 음성 저장 및 녹음 중지
-        if (e.playbackTime > 10800) {
-          stream.getAudioTracks().forEach(function (track) {
-            track.stop();
-          });
-          mediaRecorder.stop();
-          // 메서드가 호출 된 노드 연결 해제
-          analyser.disconnect();
-          audioCtx.createMediaStreamSource(stream).disconnect();
+  const onStop = useCallback((recordedBlob) => {
+    setRecordedBlob(recordedBlob);
+  }, []);
 
-          mediaRecorder.ondataavailable = function (e) {
-            setAudioUrl(e.data);
-            setOnRec(true);
-          };
-        } else {
-          setOnRec(false);
-        }
-      };
-    });
-  };
-
-  // 사용자가 음성 녹음을 중지 했을 때
-  const offRecAudio = () => {
-    // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
-    media.ondataavailable = function (e) {
-      setAudioUrl(e.data);
-      setOnRec(true);
-    };
-
-    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
-    stream.getAudioTracks().forEach(function (track) {
-      track.stop();
-    });
-
-    // 미디어 캡처 중지
-    media.stop();
-
-    // 메서드가 호출 된 노드 연결 해제
-    analyser.disconnect();
-    source.disconnect();
-
-    if (audioUrl) {
-      URL.createObjectURL(audioUrl); // 출력된 링크에서 녹음된 오디오 확인 가능
-    }
-
-    // File 생성자를 사용해 파일로 변환
-    const sound = new File([audioUrl], "soundBlob", {
-      lastModified: new Date().getTime(),
-      type: "audio",
-    });
-
-    setDisabled(false);
-    console.log(sound); // File 정보 출력
-  };
-
-  const play = () => {
-    const audio = new Audio(URL.createObjectURL(audioUrl));
-    audio.loop = false;
-    audio.volume = 1;
+  const handlePlay = useCallback(() => {
+    const url = URL.createObjectURL(recordedBlob.blob);
+    const audio = new Audio(url);
     audio.play();
-  };
+  }, [recordedBlob]);
+
+  const handleDownload = useCallback(() => {
+    if (recordedBlob && recordedBlob.blob) {
+      const { blob } = recordedBlob;
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "녹음파일.wav";
+      link.click();
+      URL.revokeObjectURL(url);
+    }
+  }, [recordedBlob]);
+
+  const isAudioAvailable = recordedBlob && recordedBlob.blob;
+  // const isRecordingInProgress = isRecording && !isAudioAvailable;
 
   return (
     <Container>
@@ -131,13 +59,49 @@ function Record() {
         <TitleBoxTitle>음성 녹음</TitleBoxTitle>
       </TitleBox>
       <ContentContainer>
-        <button onClick={onRec ? onRecAudio : offRecAudio}>녹음</button>
-        <button onClick={play} disabled={disabled}>
+        <ReactMic
+          record={isRecording}
+          onStop={onStop}
+          visualSetting="sinewave"
+          className="visualizer"
+        />
+        <RecordButton onClick={handleRecord}>{recordText}</RecordButton>
+        <PlayButton onClick={handlePlay} disabled={!isAudioAvailable}>
           재생
-        </button>
+        </PlayButton>
+        <DownloadButton onClick={handleDownload} disabled={!isAudioAvailable}>
+          다운로드
+        </DownloadButton>
       </ContentContainer>
     </Container>
   );
 }
 
 export default Record;
+
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+
+//   width: 100%;
+// `;
+// const TitleBox = styled.div`
+//   margin-top: 50px;
+//   width: 60%;
+//   border-bottom: 2px solid grey;
+//   padding-bottom: 10px;
+// `;
+// const TitleBoxTitle = styled.div`
+//   font-size: 24px;
+//   text-align: center;
+// `;
+// const ContentContainer = styled.div`
+//   font-size: 2rem;
+//   width: 100%;
+//   height: 70vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
